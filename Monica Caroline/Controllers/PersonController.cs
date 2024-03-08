7a55ce99abef09ccd6c694891b96e0a966181dd5
ManiahMellow <jellakus@gmail.com>
2024-03-08 23:26:06 +0700

    Tugas Praktikum PAA 1

---
+++ Monica Caroline/Controllers/PersonController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Monica_Caroline.Models;
+
+namespace Monica_Caroline.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost("api/person")]
+        public ActionResult<Person> ListPerson()
+        {
+            PersonContext context = new PersonContext();
+            List<Person> ListPerson = context.ListPerson();
+            return Ok(ListPerson);
+        }
+        [HttpGet("api/person/{id}")]
+        public ActionResult<Person> GetPersonById(int id)
+        {
+            PersonContext context = new PersonContext();
+            {
+                var person = context.ListPerson().Find(p => p.id_person == id);
+                if (person == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(person);
+            }
+        }
+    }
+}
---
+++ Monica Caroline/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace Monica_Caroline.Models
+{
+    public class Person
+    {
+
+        public int id_person { get; set; }
+        public string nama { get; set; }
+        public string alamat { get; set; }
+        public string email { get; set; }
+
+    }
+}
